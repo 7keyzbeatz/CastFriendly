614fee79b87555928a44c117ffc6b25578d6f88a
7Keyz Beatz <114307671+7keyzbeatz@users.noreply.github.com>
2026-02-08 08:56:35 +0200

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,5 @@
+﻿document.querySelectorAll(".liquid-card").forEach(card => {
+    const level = card.dataset.level;
+    const liquid = card.querySelector(".liquid");
+    liquid.style.setProperty("--fill", level + "%");
+});
